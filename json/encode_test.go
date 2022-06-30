0501d2012b77e917f182b54cac12dfb5acb7006e
cwinters8 <clwinters8@gmail.com>
2022-06-29 22:52:39 -0500

    return after erroring in tests

--- json/encode_test.go
+++ json/encode_test.go
@@ -11,15 +11,18 @@ func TestSimpleStringEncode(t *testing.T) {
 	got, err := json.Encode(`{"car": "Acura"}`)
 	if err != nil {
 		t.Error("encoding failed: " + err.Error())
+		return
 	}
 	want := []string{`car="Acura"`}
 	msg := fmt.Sprintf("got: %v, want: %v", got, want)
 	if len(got) != len(want) {
 		t.Error(msg)
+		return
 	}
 	for i, v := range want {
 		if v != got[i] {
 			t.Error(msg)
+			return
 		}
 	}
 }
@@ -28,15 +31,18 @@ func TestSimpleNumEncode(t *testing.T) {
 	got, err := json.Encode(`{"age": 35, "weight": 165.5}`)
 	if err != nil {
 		t.Error("encoding failed: " + err.Error())
+		return
 	}
 	want := []string{"age=35", "weight=165.5"}
 	msg := fmt.Sprintf("got: %v, want: %v", got, want)
 	if len(got) != len(want) {
 		t.Error(msg)
+		return
 	}
 	for i, v := range want {
 		if v != got[i] {
 			t.Error(msg)
+			return
 		}
 	}
 }
@@ -45,8 +51,10 @@ func TestEmptyEncode(t *testing.T) {
 	got, err := json.Encode("{}")
 	if err != nil {
 		t.Error("encoding failed: " + err.Error())
+		return
 	}
 	if len(got) != 0 {
 		t.Errorf("slice should be empty. got: %v", got)
+		return
 	}
 }
